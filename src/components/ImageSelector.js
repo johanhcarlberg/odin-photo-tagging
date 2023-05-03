8e37776779cb2b9ac83c45add5a9b6ebf90bb527
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-03 12:09:54 +0200

    Create ImageSelector component

---
+++ src/components/ImageSelector.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import '../styles/ImageSelector.css';
+
+const ImageSelector = () => {
+
+    return (
+        <div className='image-selector-wrapper'>
+            <div className='image-selector'><h1>Images</h1></div>
+        </div>
+    )
+}
+
+export default ImageSelector;
