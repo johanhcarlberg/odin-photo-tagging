b730c3c61c9e264205d94d142ba279bb0316184e
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-04 18:58:51 +0200

    Add GameBoard when state is gamePlaying

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import ImageSelect from "./ImageSelect";
 import '../styles/Game.css';
+import GameBoard from "./GameBoard";
 
 const Game = () => {
     const [gameState, setGameState] = useState('gameStart');
+    const [selectedImage, setSelectedImage] = useState(null);
 
     const onImageConfirm = (image) => {
         console.log(image);
         setGameState('gamePlaying')
+        setSelectedImage(image);
     }
     return (
         <div className="game-main">
             {gameState === 'gameStart' &&
                 <ImageSelect onConfirm={onImageConfirm}/>
+            }
+
+            {gameState === 'gamePlaying' &&
+                <GameBoard image={selectedImage}/>
             }
         </div>
     )
