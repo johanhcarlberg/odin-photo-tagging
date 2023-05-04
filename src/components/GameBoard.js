7a0418934b9e103b256e44a885bcbe427c4e2c25
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-04 18:59:13 +0200

    Add image to GameBoard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,9 +1,26 @@
-import '../styles/GameBoard.css';
+import { useEffect } from "react";
+import "../styles/GameBoard.css";
 
-const GameBoard = ({image}) => {
+const GameBoard = ({ image }) => {
+    useEffect(() => {
+        const loadImage = () => {
+            const tempImg = new Image();
+            tempImg.src = `images/${image.imageName}`;
+        };
+
+        loadImage();
+    }, [image]);
+
     return (
-        <div className="game-board"></div>
-    )
-}
+        <div className="game-board">
+            <div
+                className="game-board-image"
+                style={{
+                    backgroundImage: image && `url(images/${image.imageName})`,
+                }}
+            ></div>
+        </div>
+    );
+};
 
 export default GameBoard;
