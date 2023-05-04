a529ce6c012db6d4755c4833e2590c431b9cc0e5
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-04 17:03:41 +0200

    Add functions for switching images

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -8,6 +8,55 @@ import ObjectivesList from "./ObjectivesList";
 const ImageSelector = ({images, onConfirm}) => {
     const [currentImage, setCurrentImage] = useState(null);
 
+
+    const nextImage = () => {
+       if (images.length <= 1) {
+        return;
+       }
+
+       if (currentImage.index === images.length - 1) {
+        const nextIndex = 0;
+        setCurrentImage(
+            {
+                ...images[nextIndex],
+                index: nextIndex
+            }
+        )
+       } else {
+        const nextIndex = currentImage.index + 1;
+        setCurrentImage(
+            {
+                ...images[nextIndex],
+                index: nextIndex
+            }
+        )
+       }
+    }
+
+    const prevImage = () => {
+        if (images.length <= 1) {
+            return;
+        }
+
+        if (currentImage.index === 0) {
+            const nextIndex = images.length - 1;
+            setCurrentImage(
+                {
+                    ...images[nextIndex],
+                    index: nextIndex
+                }
+            )
+        } else {
+            const nextIndex = currentImage.index - 1;
+            setCurrentImage(
+                {
+                    ...images[nextIndex],
+                    index: nextIndex
+                }
+            )
+        }
+    }
+
     useEffect(() => {
         for (let image of images) {
             const img = new Image();
@@ -20,11 +69,15 @@ const ImageSelector = ({images, onConfirm}) => {
     });
     return (
         <div className="image-selector">
-            <button>Prev</button>
+            <button 
+                onClick={prevImage}
+                disabled={images.length <= 1}>Prev</button>
             <div className="current-image"
                 style={{backgroundImage: currentImage && `url(images/${currentImage.imageName})`}} 
             ></div>
-            <button>Next</button>
+            <button 
+            onClick={nextImage}
+            disabled={images.length <= 1}>Next</button>
             {currentImage && 
                 <ObjectivesList image={currentImage} />
             }
