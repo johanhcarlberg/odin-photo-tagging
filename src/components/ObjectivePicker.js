fe5302696229bc9829e747e7cf8104245616f279
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-05 12:38:21 +0200

    Add useEffect and ref to set position of ObjectivePicker

--- src/components/ObjectivePicker.js
+++ src/components/ObjectivePicker.js
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from 'react';
 import '../styles/ObjectivePicker.css';
 
 const ObjectivePicker = ({objectives, position}) => {
+    const ref = useRef();
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.style.left = position.x + 'px';
+            ref.current.style.top = position.y + 'px';
+        }
+    }, [])
+
     return (
-        <ul className="objective-picker" style={{left: position.x, top: position.y}}>
+        <ul ref={ref} className="objective-picker">
             {objectives.map(objective => <li key={objective.name}>{objective.name}</li>)}
         </ul>
     )
