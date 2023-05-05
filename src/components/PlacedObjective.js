41ea39ac59a452c4cd98952476d44ea63540dd68
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-05 21:05:20 +0200

    Add bounds state, update bounds of gameboard image when window resizes, use bounds to offset placed objective div

--- src/components/PlacedObjective.js
+++ src/components/PlacedObjective.js
@@ -1,16 +1,27 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../styles/PlacedObjective.css';
 
-const PlacedObjective = ({objective}) => {
+const PlacedObjective = ({objective, gameBoardImageRef}) => {
     const ref = useRef();
+    const [bounds, setBounds] = useState({});
     useEffect(() => {
-        if (ref.current) {
+        const updateBounds = () => {
+            setBounds(gameBoardImageRef.current.getBoundingClientRect());
+        }
+
+        window.addEventListener('resize', updateBounds);
+        updateBounds();
+        return () => window.removeEventListener('resize', updateBounds);
+    }, [gameBoardImageRef])
+    
+    useEffect(() => {
+        if (ref.current && gameBoardImageRef.current) {
             const width = ref.current.offsetWidth;
             const height = ref.current.offsetHeight;
-            ref.current.style.left = objective.x - width / 2 + 'px';
-            ref.current.style.top = objective.y - height / 2 + 'px';
+            ref.current.style.left = (objective.x + bounds.left) - width / 2 + 'px';
+            ref.current.style.top = (objective.y + bounds.top) - height / 2 + 'px';
         }
-    }, [objective])
+    }, [objective, bounds])
 
     return (
         <div ref={ref} className="placed-objective"></div>
