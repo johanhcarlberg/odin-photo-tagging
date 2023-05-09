da0ca05b8aec8022e90dff9e5ab85b2333dc34ec
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-09 10:53:59 +0200

    Add effect for loading bounds for objective

--- src/components/ObjectiveResult.js
+++ src/components/ObjectiveResult.js
@@ -2,22 +2,39 @@ import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 
-const ObjectiveResult = ({image, objective}) => {
-    const [bounds, setBounds] = useState([]);
+const ObjectiveResult = ({ image, objective }) => {
+    const [bounds, setBounds] = useState(null);
     useEffect(() => {
         const loadBounds = async () => {
             try {
-                const querySnapshot = await getDocs(collection(db, `images/${image.name}/objectives/${objective.id}/bounds`));
-                const boundsData = querySnapshot.docs.map(doc => doc.data());
-                console.log(boundsData);
-                setBounds(boundsData);
+                const path = `images/${image.name}/objectives/${objective.id}/bounds`;
+                console.log(path);
+                const querySnapshot = await getDocs(
+                    collection(
+                        db,
+                        path
+                    )
+                );
+                if (querySnapshot.docs.length === 0) {
+                    return;
+                }
+                const boundsData = querySnapshot.docs.map(doc => {
+                    const obj = {};
+                    const key = doc.id;
+                    obj[key] = doc.data()['value'];
+                    return obj;
+                });
+                const boundsObj = Object.assign(...boundsData);
+                setBounds(boundsObj);
             } catch (error) {
-                console.error('Error retrieving objective bounds', error);
+                console.error("Error retrieving objective bounds", error);
             }
 
-            loadBounds();
+        };
+        loadBounds();
+    }, []);
+
         }
-    }, [objective, image])
 
     return (
         <div className="objective-result"></div>
