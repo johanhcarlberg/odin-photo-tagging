84e234e49ea051a12fa0cd7dc663a48fc0cb984b
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-09 07:34:22 +0200

    Add results-content div, rename results css class to results-modal

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,8 +3,9 @@ import "../styles/Results.css";
 const Results = ({ placedObjectives }) => {
     return (
         <div class="results-wrapper">
-            <div className="results">
+            <div className="results-modal">
                 <h1>Results</h1>
+                <div className="results-content"></div>
                 <button className="primary-button">Try Again</button>
                 <button className="primary-button">Select new image</button>
             </div>
