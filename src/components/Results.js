531f402e44faa0df6ce6cae7926efb12facbef53
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-09 07:30:10 +0200

    Add header and buttons

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,7 +1,15 @@
-const Results = ({placedObjectives}) => {
+import "../styles/Results.css";
+
+const Results = ({ placedObjectives }) => {
     return (
-        <div className="results"></div>
-    )
-}
+        <div class="results-wrapper">
+            <div className="results">
+                <h1>Results</h1>
+                <button className="primary-button">Try Again</button>
+                <button className="primary-button">Select new image</button>
+            </div>
+        </div>
+    );
+};
 
 export default Results;
