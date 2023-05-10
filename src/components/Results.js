2fe0d396e47e5677131b7a1eba9e3a7339fa813e
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-10 09:26:04 +0200

    Add objective results, add onClick on Try Again button

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,21 @@
 import "../styles/Results.css";
+import ObjectiveResult from "./ObjectiveResult";
 
-const Results = ({ placedObjectives }) => {
+const Results = ({ placedObjectives, image, onTryAgain }) => {
     return (
-        <div class="results-wrapper">
+        <div className="results-wrapper">
             <div className="results-modal">
                 <h1>Results</h1>
-                <div className="results-content"></div>
-                <button className="primary-button">Try Again</button>
+                <div className="results-content">
+                    {placedObjectives.map((objective) => (
+                        <ObjectiveResult
+                            key={objective.id}
+                            objective={objective}
+                            image={image}
+                        />
+                    ))}
+                </div>
+                <button className="primary-button" onClick={onTryAgain}>Try Again</button>
                 <button className="primary-button">Select new image</button>
             </div>
         </div>
