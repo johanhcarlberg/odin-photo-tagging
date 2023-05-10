e055abcffad06318a78226c487de6b8a876d9d93
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-10 09:49:20 +0200

    Add objective result icons

--- src/components/ObjectiveResult.js
+++ src/components/ObjectiveResult.js
@@ -1,6 +1,8 @@
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
+import "../styles/ObjectiveResult.css";
+import { FaCheck, FaTimes } from "react-icons/fa";
 
 const ObjectiveResult = ({ image, objective }) => {
     const [bounds, setBounds] = useState(null);
@@ -9,19 +11,14 @@ const ObjectiveResult = ({ image, objective }) => {
             try {
                 const path = `images/${image.name}/objectives/${objective.id}/bounds`;
                 console.log(path);
-                const querySnapshot = await getDocs(
-                    collection(
-                        db,
-                        path
-                    )
-                );
+                const querySnapshot = await getDocs(collection(db, path));
                 if (querySnapshot.docs.length === 0) {
                     return;
                 }
-                const boundsData = querySnapshot.docs.map(doc => {
+                const boundsData = querySnapshot.docs.map((doc) => {
                     const obj = {};
                     const key = doc.id;
-                    obj[key] = doc.data()['value'];
+                    obj[key] = doc.data()["value"];
                     return obj;
                 });
                 const boundsObj = Object.assign(...boundsData);
@@ -29,7 +26,6 @@ const ObjectiveResult = ({ image, objective }) => {
             } catch (error) {
                 console.error("Error retrieving objective bounds", error);
             }
-
         };
         loadBounds();
     }, []);
@@ -48,12 +44,22 @@ const ObjectiveResult = ({ image, objective }) => {
         }
 
         return true;
-    }
+    };
 
-    return <div className="objective-result">
-        <p>{objective.name}</p>
-        <p>{isWithinBounds() ? 'True' : 'False'}</p>
-    </div>;
+    return (
+        <div className="objective-result">
+            <p className="objective-result-name">{objective.name}</p>
+            {bounds && (
+                <p className="objective-result-check">
+                    {isWithinBounds() ? (
+                        <FaCheck className="objective-result-true" />
+                    ) : (
+                        <FaTimes className="objective-result-false" />
+                    )}
+                </p>
+            )}
+        </div>
+    );
 };
 
 export default ObjectiveResult;
