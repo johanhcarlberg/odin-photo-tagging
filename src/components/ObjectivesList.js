b87458cdd2fdd4ac3c415727b69d546421f5ffef
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-15 19:07:39 +0200

    Fix map key

--- src/components/ObjectivesList.js
+++ src/components/ObjectivesList.js
@@ -8,9 +8,11 @@ const ObjectivesList = ({objectives}) => {
     return (
         <div className="objectives">
         <h3>Objectives</h3>
+            <div className="objectives-list">
             {objectives.map(objective => {
-                return <p key={objective.name}>{objective.name}</p>
+                return <p key={objective.id}>{objective.name}</p>
             })}
+            </div>
         </div>
     )
 }
