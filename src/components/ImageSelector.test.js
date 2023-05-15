445d827c88eba36772c111c21b9ff5de1fd325ad
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-15 15:23:03 +0200

    Change from 'test' to 'it'

--- src/components/ImageSelector.test.js
+++ src/components/ImageSelector.test.js
@@ -2,20 +2,20 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ImageSelector from "./ImageSelector";
 
-jest.mock('../effects/useObjectives', () => {
+jest.mock("../effects/useObjectives", () => {
     return function DummyUseObjectives(props) {
         return [];
-    }
-})
+    };
+});
 
-test("renders", async () => {
+it("renders", async () => {
     const images = [];
     const onConfirm = jest.fn();
 
     render(<ImageSelector images={images} onConfirm={onConfirm} />);
 });
 
-test("can click confirm button", async () => {
+it("can click confirm button", async () => {
     const images = [{ imageName: "beach.png", name: "Beach" }];
     const onConfirm = jest.fn();
     const user = userEvent.setup();
@@ -28,24 +28,24 @@ test("can click confirm button", async () => {
     expect(onConfirm).toBeCalled();
 });
 
-test("displays image name", async () => {
+it("displays image name", async () => {
     const images = [{ imageName: "beach.png", name: "Beach" }];
     const onConfirm = jest.fn();
 
     render(<ImageSelector images={images} onConfirm={onConfirm} />);
-    const imageName = screen.getByText(images[0].name);
+    screen.getByText(images[0].name);
 });
 
-test("displays image", async () => {
+it("displays image", async () => {
     const images = [{ imageName: "beach.png", name: "Beach" }];
     const onConfirm = jest.fn();
 
     render(<ImageSelector images={images} onConfirm={onConfirm} />);
-    const image = screen.getByLabelText('current-image');
+    const image = screen.getByLabelText("current-image");
     expect(image.style.backgroundImage).toContain(images[0].imageName);
-})
+});
 
-test("can change image", async () => {
+it("can change image", async () => {
     const images = [
         { imageName: "beach.png", name: "Beach" },
         { imageName: "park.png", name: "Park" },
@@ -61,7 +61,7 @@ test("can change image", async () => {
     const prevButton = screen.getByRole("button", {
         name: "previous-image-button",
     });
-    const image = screen.getByLabelText('current-image');
+    const image = screen.getByLabelText("current-image");
     screen.getByText(images[0].name);
     expect(image.style.backgroundImage).toContain(images[0].imageName);
     await user.click(nextButton);
