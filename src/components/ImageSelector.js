797c2f328baaad205ceb4a90dc5af582e6484d7a
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-15 09:56:27 +0200

    Add name for currently selected image

--- src/components/ImageSelector.js
+++ src/components/ImageSelector.js
@@ -1,38 +1,33 @@
 import { useEffect, useState } from "react";
-import '../styles/ImageSelector.css';
+import "../styles/ImageSelector.css";
 
-import { collection, getDocs, where, query, doc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { collection, getDocs, where, query, doc } from "firebase/firestore";
+import { db } from "../firebase";
 import ObjectivesList from "./ObjectivesList";
 import useObjectives from "../effects/useObjectives";
 
-const ImageSelector = ({images, onConfirm}) => {
+const ImageSelector = ({ images, onConfirm }) => {
     const [currentImage, setCurrentImage] = useState(null);
 
-
     const nextImage = () => {
-       if (images.length <= 1) {
-        return;
-       }
+        if (images.length <= 1) {
+            return;
+        }
 
-       if (currentImage.index === images.length - 1) {
-        const nextIndex = 0;
-        setCurrentImage(
-            {
+        if (currentImage.index === images.length - 1) {
+            const nextIndex = 0;
+            setCurrentImage({
                 ...images[nextIndex],
-                index: nextIndex
-            }
-        )
-       } else {
-        const nextIndex = currentImage.index + 1;
-        setCurrentImage(
-            {
+                index: nextIndex,
+            });
+        } else {
+            const nextIndex = currentImage.index + 1;
+            setCurrentImage({
                 ...images[nextIndex],
-                index: nextIndex
-            }
-        )
-       }
-    }
+                index: nextIndex,
+            });
+        }
+    };
 
     const prevImage = () => {
         if (images.length <= 1) {
@@ -41,22 +36,18 @@ const ImageSelector = ({images, onConfirm}) => {
 
         if (currentImage.index === 0) {
             const nextIndex = images.length - 1;
-            setCurrentImage(
-                {
-                    ...images[nextIndex],
-                    index: nextIndex
-                }
-            )
+            setCurrentImage({
+                ...images[nextIndex],
+                index: nextIndex,
+            });
         } else {
             const nextIndex = currentImage.index - 1;
-            setCurrentImage(
-                {
-                    ...images[nextIndex],
-                    index: nextIndex
-                }
-            )
+            setCurrentImage({
+                ...images[nextIndex],
+                index: nextIndex,
+            });
         }
-    }
+    };
 
     useEffect(() => {
         for (let image of images) {
@@ -65,12 +56,10 @@ const ImageSelector = ({images, onConfirm}) => {
         }
 
         if (images.length > 0) {
-            setCurrentImage(
-                {
-                    ...images[0],
-                    index: 0
-                }
-            );
+            setCurrentImage({
+                ...images[0],
+                index: 0,
+            });
         }
     }, [images]);
 
@@ -78,26 +67,42 @@ const ImageSelector = ({images, onConfirm}) => {
 
     return (
         <div className="image-selector">
-            <button 
+            {currentImage && (
+                <h3 className="current-image-name">{currentImage.name}</h3>
+            )}
+            <button
+                className="previous-image-button"
                 onClick={prevImage}
-                disabled={images.length <= 1}>Prev</button>
-            <div className="current-image"
-                style={{backgroundImage: currentImage && `url(images/${currentImage.imageName})`}} 
+                disabled={images.length <= 1}
+            >
+                Prev
+            </button>
+            <div
+                className="current-image"
+                style={{
+                    backgroundImage:
+                        currentImage && `url(images/${currentImage.imageName})`,
+                }}
             ></div>
-            <button 
-            onClick={nextImage}
-            disabled={images.length <= 1}>Next</button>
-            {objectives.length > 0 && 
+            <button
+                className="next-image-button"
+                onClick={nextImage}
+                disabled={images.length <= 1}
+            >
+                Next
+            </button>
+            {objectives.length > 0 && (
                 <ObjectivesList objectives={objectives} />
-            }
+            )}
             <button
                 className="confirm-button"
                 disabled={!currentImage}
-                onClick={() => onConfirm(currentImage)}>
+                onClick={() => onConfirm(currentImage)}
+            >
                 Confirm
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default ImageSelector;
