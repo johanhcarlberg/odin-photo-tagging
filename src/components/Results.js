d02def11a62febfdcd98e2587695d631dbc516ab
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-17 19:25:06 +0200

    Add time display

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,22 @@
 import "../styles/Results.css";
 import ObjectiveResult from "./ObjectiveResult";
+import useTime from "../effects/useTime";
 
-const Results = ({ placedObjectives, image, onTryAgain, onSelectNewImage }) => {
+const Results = ({
+    placedObjectives,
+    image,
+    onTryAgain,
+    onSelectNewImage,
+    startTime,
+}) => {
+    const endTime = useTime();
+    const timeToDisplay = (endTime - startTime) / 1000;
+
     return (
         <div className="results-wrapper">
             <div className="results-modal">
                 <h1>Results</h1>
+                <h3 className="time-elapsed">Time elapsed: {timeToDisplay}s</h3>
                 <div className="results-content">
                     {placedObjectives.map((objective) => (
                         <ObjectiveResult
