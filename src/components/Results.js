477c6b68c77a2930b1ec7461da99ce47801f9840
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-17 19:40:27 +0200

    Add highscore name input

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,7 @@
 import "../styles/Results.css";
 import ObjectiveResult from "./ObjectiveResult";
 import useTime from "../effects/useTime";
+import { useState } from "react";
 
 const Results = ({
     placedObjectives,
@@ -9,6 +10,7 @@ const Results = ({
     onSelectNewImage,
     startTime,
 }) => {
+    const [highscoreName, setHighscoreName] = useState("");
     const endTime = useTime();
     const timeToDisplay = (endTime - startTime) / 1000;
 
@@ -26,6 +28,19 @@ const Results = ({
                         />
                     ))}
                 </div>
+                <label
+                    htmlFor="highscore-name"
+                    className="highscore-name-label"
+                >
+                    Highscore name
+                </label>
+                <input
+                    name="highscore-name"
+                    aria-label="highscore-name"
+                    className="highscore-name"
+                    value={highscoreName}
+                    onChange={(e) => setHighscoreName(e.target.value)}
+                />
                 <button
                     className="primary-button"
                     aria-label="try-again-button"
