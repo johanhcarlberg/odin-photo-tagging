ca394f6e7abb416a5d7aa2589cdf981768a8662c
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-21 19:24:48 +0200

    Add logic for displaying highscores in results modal

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import useHighscores from "../effects/useHighscores";
+import Highscores from "./Highscores";
 
 const Results = ({
     placedObjectives,
@@ -22,6 +23,7 @@ const Results = ({
         placedObjectives
     );
     const [submitDisabled, setSubmitDisabled] = useState(false);
+    const [showHighscores, setShowHighscores] = useState(false);
 
     const onHighscoreSubmit = async (name) => {
         console.log(name);
@@ -45,56 +47,65 @@ const Results = ({
 
         return highscores[highscores.length - 1] > timeToDisplay;
     };
-
     return (
         <div className="results-wrapper">
             <div className="results-modal">
-                <h1>Results</h1>
-                <h3 className="time-elapsed">Time elapsed: {timeToDisplay}s</h3>
-                <div className="results-content">
-                    {isLoading === false &&
-                        objectiveResults.map((objective) => {
-                            return (
-                                <ObjectiveResult
-                                    key={objective.id}
-                                    objective={objective}
+                {showHighscores ? (
+                    <Highscores
+                        image={image}
+                        onClose={() => setShowHighscores(false)}
+                    />
+                ) : (
+                    <div>
+                        <h1>Results</h1>
+                        <h3 className="time-elapsed">
+                            Time elapsed: {timeToDisplay}s
+                        </h3>
+                        <div className="results-content">
+                            {isLoading === false &&
+                                objectiveResults.map((objective) => {
+                                    return (
+                                        <ObjectiveResult
+                                            key={objective.id}
+                                            objective={objective}
+                                        />
+                                    );
+                                })}
+                        </div>
+                        {objectiveResults.filter((obj) => obj.result === true)
+                            .length === placedObjectives.length &&
+                            placedObjectives.length > 0 &&
+                            isTimeTopTen() && (
+                                <HighscoreSubmit
+                                    onSubmit={onHighscoreSubmit}
+                                    submitDisabled={submitDisabled}
                                 />
-                            );
-                        })}
-                </div>
-
-                {objectiveResults.filter((obj) => obj.result === true)
-                    .length === placedObjectives.length &&
-                    placedObjectives.length > 0 &&
-                    isTimeTopTen() && (
-                        <HighscoreSubmit
-                            onSubmit={onHighscoreSubmit}
-                            submitDisabled={submitDisabled}
-                        />
-                    )}
-
-                <div class="results-buttons">
-                    <button
-                        className="primary-button"
-                        aria-label="try-again-button"
-                        onClick={onTryAgain}
-                    >
-                        Try Again
-                    </button>
-                    <button
-                        className="primary-button"
-                        aria-label="select-new-image-button"
-                        onClick={onSelectNewImage}
-                    >
-                        Select new image
-                    </button>
-                    <button
-                        className="show-scores-button primary-button"
-                        aria-label="show-scores-button"
-                    >
-                        Show High Scores
-                    </button>
-                </div>
+                            )}
+                        <div className="results-buttons">
+                            <button
+                                className="primary-button"
+                                aria-label="try-again-button"
+                                onClick={onTryAgain}
+                            >
+                                Try Again
+                            </button>
+                            <button
+                                className="primary-button"
+                                aria-label="select-new-image-button"
+                                onClick={onSelectNewImage}
+                            >
+                                Select new image
+                            </button>
+                            <button
+                                className="show-scores-button primary-button"
+                                aria-label="show-scores-button"
+                                onClick={() => setShowHighscores(true)}
+                            >
+                                Show High Scores
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
