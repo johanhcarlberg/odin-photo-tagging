623796bd20a6f67fe5a564703c4b7f9881ba179e
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-21 19:25:59 +0200

    Add classnames

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,22 +1,39 @@
 import useHighscores from "../effects/useHighscores";
+import "../styles/Highscores.css";
 
 const Highscores = ({ image, onClose }) => {
     const highscores = useHighscores(image);
     return (
         <div className="highscores">
-            <h3>High Scores</h3>
-            <button onClick={onClose} aria-label="close-button-corner">
+            <h2>High Scores</h2>
+            <button
+                onClick={onClose}
+                className="close-button-corner"
+                aria-label="close-button-corner"
+            >
                 Close
             </button>
+            <div className="highscores-header">
+                <p className="highscore-name-header">Name</p>
+                <p className="highscore-time-header">Time</p>
+            </div>
             {highscores.map((score) => {
                 return (
                     <div className="highscores-item" key={score.id}>
-                        <p className="highscore-name">{score.name}</p>
-                        <p className="highscore-time">{score.time + "s"}</p>
+                        <p className="highscore-item-name">
+                            {score.name || "Unknown"}
+                        </p>
+                        <p className="highscore-item-time">
+                            {score.time + "s"}
+                        </p>
                     </div>
                 );
             })}
-            <button onClick={onClose} aria-label="close-button">
+            <button
+                onClick={onClose}
+                className="close-button primary-button"
+                aria-label="close-button"
+            >
                 Close
             </button>
         </div>
