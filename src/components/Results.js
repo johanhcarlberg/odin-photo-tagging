40afb539efa65c04e6274aec08b7dff93f1482a7
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-21 18:56:25 +0200

    Add functionality for submitting scores, add show high scores button

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,6 +3,10 @@ import ObjectiveResult from "./ObjectiveResult";
 import useTime from "../effects/useTime";
 import useObjectiveResults from "../effects/useObjectiveResults";
 import HighscoreSubmit from "./HighscoreSubmit";
+import { useEffect, useState } from "react";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import useHighscores from "../effects/useHighscores";
 
 const Results = ({
     placedObjectives,
@@ -17,9 +21,30 @@ const Results = ({
         image,
         placedObjectives
     );
-    const onHighscoreSubmit = () => {
-        console.log('submit highscore');
-    }
+    const [submitDisabled, setSubmitDisabled] = useState(false);
+
+    const onHighscoreSubmit = async (name) => {
+        console.log(name);
+        if (!isTimeTopTen()) {
+            return;
+        }
+        setSubmitDisabled(true);
+        const path = `images/${image.name}/highscores`;
+        const docRef = await addDoc(collection(db, path), {
+            name: name || "Unknown",
+            time: timeToDisplay,
+        });
+    };
+
+    const highscores = useHighscores(image);
+
+    const isTimeTopTen = () => {
+        if (highscores.length < 10) {
+            return true;
+        }
+
+        return highscores[highscores.length - 1] > timeToDisplay;
+    };
 
     return (
         <div className="results-wrapper">
@@ -37,26 +62,39 @@ const Results = ({
                             );
                         })}
                 </div>
-                
-                {(objectiveResults.filter((obj) => obj.result === true)
-                    .length === placedObjectives.length && placedObjectives.length > 0) && (
-                    <HighscoreSubmit onSubmit={onHighscoreSubmit}/>
-                )}
 
-                <button
-                    className="primary-button"
-                    aria-label="try-again-button"
-                    onClick={onTryAgain}
-                >
-                    Try Again
-                </button>
-                <button
-                    className="primary-button"
-                    aria-label="select-new-image-button"
-                    onClick={onSelectNewImage}
-                >
-                    Select new image
-                </button>
+                {objectiveResults.filter((obj) => obj.result === true)
+                    .length === placedObjectives.length &&
+                    placedObjectives.length > 0 &&
+                    isTimeTopTen() && (
+                        <HighscoreSubmit
+                            onSubmit={onHighscoreSubmit}
+                            submitDisabled={submitDisabled}
+                        />
+                    )}
+
+                <div class="results-buttons">
+                    <button
+                        className="primary-button"
+                        aria-label="try-again-button"
+                        onClick={onTryAgain}
+                    >
+                        Try Again
+                    </button>
+                    <button
+                        className="primary-button"
+                        aria-label="select-new-image-button"
+                        onClick={onSelectNewImage}
+                    >
+                        Select new image
+                    </button>
+                    <button
+                        className="show-scores-button primary-button"
+                        aria-label="show-scores-button"
+                    >
+                        Show High Scores
+                    </button>
+                </div>
             </div>
         </div>
     );
