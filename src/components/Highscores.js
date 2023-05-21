0381c0a5eff0b85993d52713ad5ac6bfad84869e
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-21 19:13:19 +0200

    Add close buttons

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,19 +1,26 @@
 import useHighscores from "../effects/useHighscores";
 
-const Highscores = ({image}) => {
+const Highscores = ({ image, onClose }) => {
     const highscores = useHighscores(image);
     return (
         <div className="highscores">
-            {highscores.map(score => {
+            <h3>High Scores</h3>
+            <button onClick={onClose} aria-label="close-button-corner">
+                Close
+            </button>
+            {highscores.map((score) => {
                 return (
                     <div className="highscores-item" key={score.id}>
                         <p className="highscore-name">{score.name}</p>
-                        <p className="highscore-time">{score.time + 's'}</p>
+                        <p className="highscore-time">{score.time + "s"}</p>
                     </div>
-                )
+                );
             })}
+            <button onClick={onClose} aria-label="close-button">
+                Close
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Highscores;
